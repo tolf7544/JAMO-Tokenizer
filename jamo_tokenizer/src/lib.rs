b6ff579f16ce2cc9e8410ec502c437eac58d2eaa
yjlee06 <67129370+tolf7544@users.noreply.github.com>
2025-06-05 06:20:21 +0000

    maturin enviroment ssetting

---
+++ jamo_tokenizer/src/lib.rs
@@ -0,0 +1,30 @@
+use pyo3::prelude::*;
+
+
+// https://pyo3.rs/v0.18.1/module
+
+/*
+tokenize
+combine
+jamo_format
+encoding
+decoding
+vocab
+TokenizerTrain
+ComputeTokenizer  <  다형성 기반 >
+TrainTokenizer < 다형성 기반 >
+Tokenizer
+*/
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn jamo_tokenizer(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
